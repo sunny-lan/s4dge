94ab17c948c2e80aa18dbacda3d505718b1cb4f7
sunny-lan <sunny.lan@uwaterloo.ca>
2022-10-17 18:02:58 -0400

    Clean up code a bit

--- Assets/v2/LoadShapeFromFile.cs
+++ Assets/v2/LoadShapeFromFile.cs
@@ -1,3 +1,4 @@
+using OpenCover.Framework.Model;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,10 @@ using v2;
 
 public class LoadShapeFromFile : MonoBehaviour
 {
+    public string filePath= "Assets/Models/inclinedPlaneModel.s4dge";
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<SliceRenderer>().shape = new InterpolationBasedShape("Assets/Models/inclinedPlaneModel.s4dge");
+        GetComponent<SliceRenderer>().shape = new InterpolationBasedShape(filePath);
     }
 }
