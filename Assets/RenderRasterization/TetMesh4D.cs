bff9aff3bf0656926b2066bf8f47eeeccb327772
Royi Luo <r34luo@uwaterloo.ca>
2023-06-13 20:24:54 -0400

    Add Transform4D class for Rasterization Renderer

--- Assets/RenderRasterization/TetMesh4D.cs
+++ Assets/RenderRasterization/TetMesh4D.cs
@@ -2,59 +2,66 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class TetMesh4D : MonoBehaviour
+namespace RasterizationRenderer
 {
-    public Mesh mesh = new();
 
-    // Make sure struct in passed in correct layout to the mesh vertex buffer
-    [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
-    public struct VertexData
+    public class TetMesh4D : MonoBehaviour
     {
-        public Vector4 position;
-        public Vector4 normal;
-    }
+        public Mesh mesh = new();
+        public Transform4D modelWorldTransform4D = new();
+        public Matrix4x4 modelViewProjection3D = Matrix4x4.identity;
 
-    public class Tet4D
-    {
-        public int[] tetPoints; // points to indices in Vector4 points array
-
-        public Tet4D(int[] tetPoints)
+        // Make sure struct in passed in correct layout to the mesh vertex buffer
+        [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
+        public struct VertexData
         {
-            this.tetPoints = tetPoints;
+            public Vector4 position;
+            public Vector4 normal;
+        }
+
+        public class Tet4D
+        {
+            public int[] tetPoints; // points to indices in Vector4 points array
+
+            public Tet4D(int[] tetPoints)
+            {
+                this.tetPoints = tetPoints;
+            }
+        }
+
+        // Updates the mesh based on the vertices, tetrahedra
+        public void UpdateMesh(VertexData[] vertices, Tet4D[] tets)
+        {
+            mesh.Clear();
+
+            // Override vertex buffer params so that position, normal take in 4D vectors
+            mesh.SetVertexBufferParams(
+                vertices.Length,
+                new[]
+                {
+                new VertexAttributeDescriptor(VertexAttribute.Position, VertexAttributeFormat.Float32, 4),
+                new VertexAttributeDescriptor(VertexAttribute.Normal, VertexAttributeFormat.Float32, 4),
+                }
+            );
+
+            // Set vertices, normals for the mesh
+            mesh.SetVertexBufferData(vertices, 0, 0, vertices.Length);
+
+            // Set tetrahedra vertex indices for mesh
+            mesh.SetIndices(tets.SelectMany(tet => tet.tetPoints).ToArray(), MeshTopology.Quads, 0);
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
         }
     }
 
-    // Updates the mesh based on the vertices, tetrahedra
-    public void UpdateMesh(VertexData[] vertices, Tet4D[] tets)
-    {
-        mesh.Clear();
-
-        // Override vertex buffer params so that position, normal take in 4D vectors
-        mesh.SetVertexBufferParams(
-            vertices.Length,
-            new[]
-            {
-                new VertexAttributeDescriptor(VertexAttribute.Position, VertexAttributeFormat.Float32, 4),
-                new VertexAttributeDescriptor(VertexAttribute.Normal, VertexAttributeFormat.Float32, 4),
-            }
-        );
-
-        // Set vertices, normals for the mesh
-        mesh.SetVertexBufferData(vertices, 0, 0, vertices.Length);
-
-        // Set tetrahedra vertex indices for mesh
-        mesh.SetIndices(tets.SelectMany(tet => tet.tetPoints).ToArray(), MeshTopology.Quads, 0);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
