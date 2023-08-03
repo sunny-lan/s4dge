0d3ec276444ca36e0cc73c38cf2a4da77a917c37
sunny-lan <sunny.lan@uwaterloo.ca>
2023-08-03 15:49:03 -0400

    Add demo for noodle algorithm

---
+++ Assets/Rasterize3Helix.cs
@@ -0,0 +1,60 @@
+using RasterizationRenderer;
+using UnityEngine;
+
+public class Rasterize3Helix : MonoBehaviour
+{
+    TetMeshRenderer4D tetMeshRenderer;
+    TriangleMesh triMesh;
+
+    public float thickness;
+    public float zSliceStart, zSliceLength, zSliceInterval;
+    public float samplingInterval;
+
+    void Awake()
+    {
+        tetMeshRenderer = GetComponent<TetMeshRenderer4D>();
+        triMesh = GetComponent<TriangleMesh>();
+    }
+
+    private void Start()
+    {
+        var line = new ParametricShape1D()
+        {
+            Divisions = 1/samplingInterval,
+            End = 2 * Mathf.PI,
+            Start = 0,
+            Path = s =>
+            {
+                return new(
+                    2 * Mathf.Sin(s),
+                    2 * Mathf.Cos(s),
+                    s, 0
+                );
+            }
+        };
+
+        var converted = ManifoldConverter.HyperCylinderify(line, s => thickness);
+
+        var mesh = MeshGenerator4D.GenerateTetMesh(converted.Equation, converted.Normal, converted.Bounds);
+
+        tetMeshRenderer.SetTetMesh(mesh);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tetMeshRenderer.Render(zSliceStart, zSliceLength, zSliceInterval);
+    }
+
+    private void OnEnable()
+    {
+        tetMeshRenderer.gameObject.SetActive(true);
+        triMesh.gameObject.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        tetMeshRenderer.gameObject.SetActive(false);
+        triMesh.gameObject.SetActive(false);
+    }
+}
