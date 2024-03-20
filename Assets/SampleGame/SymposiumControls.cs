ffbdeba93e2ab978490a16999ae35b1741c6d51e
Luke Klassen <lklassen13@gmail.com>
2024-03-19 20:16:29 -0400

    Working version of Hypercone demo

--- Assets/SampleGame/SymposiumControls.cs
+++ Assets/SampleGame/SymposiumControls.cs
@@ -25,6 +25,8 @@ public class SymposiumControls : MonoBehaviour
         t4d = GetComponent<Transform4D>();
         UnityEngine.Cursor.lockState = CursorLockMode.Locked;
         UnityEngine.Cursor.visible = false;
+
+        ResetLookRotation();
     }
 
     Vector2 lookRotation; //x=side to side rotation, y=up down rotation
@@ -32,9 +34,25 @@ public class SymposiumControls : MonoBehaviour
     public float maxStrafeSpeed = 1f;
     private bool rotateIn4D = false;
 
+    void ResetLookRotation()
+    {
+        // Update look rotation back to the corresponding rotation
+        if (!rotateIn4D) {
+            // camera rotates left/right, up/down
+            lookRotation.x = t4d.localRotation[(int)Rot4D.xw];
+            lookRotation.y = t4d.localRotation[(int)Rot4D.yw];
+            lookMode.text = "Rotation Planes = 3D (xy, xw)";
+        } else {
+            // camera rotates left/right, up/down
+            lookRotation.x = t4d.localRotation[(int)Rot4D.xz];
+            lookRotation.y = t4d.localRotation[(int)Rot4D.yz];
+            lookMode.text = "Rotation Planes = 4D (xz, yz)";
+        }
+    }
+
     void Update()
     {
-        if (Camera4D.main != null)
+        if (Camera4D.main != null && debugCam != null)
         {
             debugCam.text = "World to Camera\n" + Camera4D.main.WorldToCameraTransform.ToString() + "\n\nCamera to World\n" + Camera4D.main.CameraToWorldTransform.ToString();
             // Debug.Log(Camera4D.main.CameraToWorldTransform.ToString());
@@ -52,18 +70,7 @@ public class SymposiumControls : MonoBehaviour
         {
             rotateIn4D = !rotateIn4D;
 
-            // Update look rotation back to the corresponding rotation
-            if (!rotateIn4D) {
-                // camera rotates left/right, up/down
-                lookRotation.x = t4d.localRotation[(int)Rot4D.xw];
-                lookRotation.y = -t4d.localRotation[(int)Rot4D.yw];
-                lookMode.text = "Rotation Planes = 3D (xy, xw)";
-            } else {
-                // camera rotates left/right, up/down
-                lookRotation.x = t4d.localRotation[(int)Rot4D.xz];
-                lookRotation.y = -t4d.localRotation[(int)Rot4D.yz];
-                lookMode.text = "Rotation Planes = 4D (xz, yz)";
-            }
+            ResetLookRotation();
         }
 
         // limit rotation to valid stuff
@@ -75,22 +82,22 @@ public class SymposiumControls : MonoBehaviour
         if (!rotateIn4D) {
             // camera rotates left/right, up/down
             t4d.localRotation[(int)Rot4D.xw] = lookRotation.x;
-            t4d.localRotation[(int)Rot4D.yw] = -lookRotation.y;
+            t4d.localRotation[(int)Rot4D.yw] = lookRotation.y;
         } else {
             // camera rotates left/right, up/down
             t4d.localRotation[(int)Rot4D.xz] = lookRotation.x;
-            t4d.localRotation[(int)Rot4D.yz] = -lookRotation.y;
+            t4d.localRotation[(int)Rot4D.yz] = lookRotation.y;
         }
 
         // wasd movement
         Vector4 wasdDirection = Vector4.zero;
         if (Input.GetKey(KeyCode.W))
         {
-            wasdDirection += new Vector4(0, 1, 0, 0);
+            wasdDirection += new Vector4(0, 0, 0, 1);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            wasdDirection += new Vector4(0, -1, 0, 0);
+            wasdDirection += new Vector4(0, 0, 0, -1);
         }
         if (Input.GetKey(KeyCode.A))
         {
@@ -118,11 +125,11 @@ public class SymposiumControls : MonoBehaviour
         // Up/down and forwards/backward not dependent on rotation positioned
         if (Input.GetKey(KeyCode.Q))
         {
-            t4d.localPosition += new Vector4(0, 0, 0, 1) * maxStrafeSpeed * Time.deltaTime;
+            t4d.localPosition += new Vector4(0, 1, 0, 0) * maxStrafeSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.E))
         {
-            t4d.localPosition += new Vector4(0, 0, 0, -1) * maxStrafeSpeed * Time.deltaTime;
+            t4d.localPosition += new Vector4(0, -1, 0, 0) * maxStrafeSpeed * Time.deltaTime;
         }
         
         if (Input.GetKey(KeyCode.LeftShift))
