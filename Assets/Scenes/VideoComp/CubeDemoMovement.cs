ccb774fb5eb2572b308106b7f8bed7855de42028
sunny-lan <sunny.lan@uwaterloo.ca>
2024-03-26 09:27:57 -0400

    cube demo

--- Assets/Scenes/VideoComp/CubeDemoMovement.cs
+++ Assets/Scenes/VideoComp/CubeDemoMovement.cs
@@ -9,16 +9,17 @@ public class CubeDemoMovement : MonoBehaviour
 
 	// Start is called before the first frame update
 	void Start()
-    {
-        t4d=GetComponent<Transform4D>();
-        StartCoroutine(run());
-    }
+	{
+		t4d = GetComponent<Transform4D>();
+		StartCoroutine(run());
+	}
 
-    IEnumerator run()
-    {
-		var rate = 0.5f;
+	IEnumerator run()
+	{
+		var rate = 1.2f;
 		var waitBetween = 1f;
-        if(false){
+		if (true)
+		{
 			yield return new WaitForSeconds(waitBetween);
 
 			for (float prog = 0; prog <= 1; prog += 3 / 360f)
@@ -61,7 +62,7 @@ public class CubeDemoMovement : MonoBehaviour
 				yield return new WaitForFixedUpdate();
 			}
 		}
-		if (false)
+		if (true)
 		{
 			yield return new WaitForSeconds(waitBetween);
 
@@ -154,6 +155,171 @@ public class CubeDemoMovement : MonoBehaviour
 				float rad = Mathf.Lerp(0, 2 * Mathf.PI, prog);
 				//xy, xw
 				t4d.localRotation[(int)Rot4D.yw] = t4d.localRotation[(int)Rot4D.zw] = rad;
+				yield return new WaitForFixedUpdate();
+			}
+		}
+		if (true)
+		{
+
+			yield return new WaitForSeconds(waitBetween);
+
+			for (float prog = 0; prog <= 1; prog += rate / 360f)
+			{
+				float rad = Mathf.Lerp(0, 2 * Mathf.PI, prog);
+				//xw, yw
+				t4d.localRotation[(int)Rot4D.xz] = t4d.localRotation[(int)Rot4D.yz] = rad;
+				yield return new WaitForFixedUpdate();
+			}
+
+			yield return new WaitForSeconds(waitBetween);
+
+			for (float prog = 0; prog <= 1; prog += rate / 360f)
+			{
+				float rad = Mathf.Lerp(0, 2 * Mathf.PI, prog);
+				//xw, zw
+				t4d.localRotation[(int)Rot4D.xz] = t4d.localRotation[(int)Rot4D.zw] = rad;
+				yield return new WaitForFixedUpdate();
+			}
+
+			yield return new WaitForSeconds(waitBetween);
+
+			for (float prog = 0; prog <= 1; prog += rate / 360f)
+			{
+				float rad = Mathf.Lerp(0, 2 * Mathf.PI, prog);
+				//yw, zw
+				t4d.localRotation[(int)Rot4D.yz] = t4d.localRotation[(int)Rot4D.zw] = rad;
+				yield return new WaitForFixedUpdate();
+			}
+		}
+		
+		//double rotations
+		if (true)
+		{
+
+			yield return new WaitForSeconds(waitBetween);
+
+			for (float prog = 0; prog <= 1; prog += rate / 360f)
+			{
+				float rad = Mathf.Lerp(0, 2 * Mathf.PI, prog);
+				//xy, zw
+				t4d.localRotation[(int)Rot4D.xy] = t4d.localRotation[(int)Rot4D.zw] = rad;
+				yield return new WaitForFixedUpdate();
+			}
+
+			yield return new WaitForSeconds(waitBetween);
+
+			for (float prog = 0; prog <= 1; prog += rate / 360f)
+			{
+				float rad = Mathf.Lerp(0, 2 * Mathf.PI, prog);
+				//yz, xw
+				t4d.localRotation[(int)Rot4D.yw] = t4d.localRotation[(int)Rot4D.xz] = rad;
+				yield return new WaitForFixedUpdate();
+			}
+
+			yield return new WaitForSeconds(waitBetween);
+
+			for (float prog = 0; prog <= 1; prog += rate / 360f)
+			{
+				float rad = Mathf.Lerp(0, 2 * Mathf.PI, prog);
+				//xz, yw
+				t4d.localRotation[(int)Rot4D.xw] = t4d.localRotation[(int)Rot4D.yz] = rad;
+				yield return new WaitForFixedUpdate();
+			}
+		}
+
+		// 3 plane rotations
+		if (true)
+		{
+			//xy, xz, xw
+			//xy xw yw
+			//xw xz yz
+			//xy yw zw
+
+			yield return new WaitForSeconds(waitBetween);
+
+			for (float prog = 0; prog <= 1; prog += rate / 360f)
+			{
+				float rad = Mathf.Lerp(0, 2 * Mathf.PI, prog);
+				t4d.localRotation[(int)Rot4D.xy] = t4d.localRotation[(int)Rot4D.xw] = t4d.localRotation[(int)Rot4D.xz] = rad;
+				yield return new WaitForFixedUpdate();
+			}
+
+
+			yield return new WaitForSeconds(waitBetween);
+
+			for (float prog = 0; prog <= 1; prog += rate / 360f)
+			{
+				float rad = Mathf.Lerp(0, 2 * Mathf.PI, prog);
+				t4d.localRotation[(int)Rot4D.xy] = t4d.localRotation[(int)Rot4D.xz] = t4d.localRotation[(int)Rot4D.yz] = rad;
+				yield return new WaitForFixedUpdate();
+			}
+
+
+
+			yield return new WaitForSeconds(waitBetween);
+
+			for (float prog = 0; prog <= 1; prog += rate / 360f)
+			{
+				float rad = Mathf.Lerp(0, 2 * Mathf.PI, prog);
+				t4d.localRotation[(int)Rot4D.xz] = t4d.localRotation[(int)Rot4D.xw] = t4d.localRotation[(int)Rot4D.yw] = rad;
+				yield return new WaitForFixedUpdate();
+			}
+
+
+
+			yield return new WaitForSeconds(waitBetween);
+
+			for (float prog = 0; prog <= 1; prog += rate / 360f)
+			{
+				float rad = Mathf.Lerp(0, 2 * Mathf.PI, prog);
+				t4d.localRotation[(int)Rot4D.xy] = t4d.localRotation[(int)Rot4D.yz] = t4d.localRotation[(int)Rot4D.zw] = rad;
+				yield return new WaitForFixedUpdate();
+			}
+		}
+
+		// 4 plane
+		if (true)
+		{
+
+			// xy yz xz xw
+			yield return new WaitForSeconds(waitBetween);
+
+			for (float prog = 0; prog <= 1; prog += rate / 360f)
+			{
+				float rad = Mathf.Lerp(0, 2 * Mathf.PI, prog);
+				t4d.localRotation[(int)Rot4D.xy] = t4d.localRotation[(int)Rot4D.yw] = t4d.localRotation[(int)Rot4D.xw] = t4d.localRotation[(int)Rot4D.xz] = rad;
+				yield return new WaitForFixedUpdate();
+			}
+		}
+
+
+		// 5 plane
+		if (true)
+		{
+
+			// xy yz xz xw yw
+			yield return new WaitForSeconds(waitBetween);
+
+			for (float prog = 0; prog <= 1; prog += rate / 360f)
+			{
+				float rad = Mathf.Lerp(0, 2 * Mathf.PI, prog);
+				t4d.localRotation[(int)Rot4D.xy] = t4d.localRotation[(int)Rot4D.yw] = t4d.localRotation[(int)Rot4D.xw] = t4d.localRotation[(int)Rot4D.xz] = t4d.localRotation[(int)Rot4D.yz] = rad;
+				yield return new WaitForFixedUpdate();
+			}
+		}
+
+
+		// 6 plane
+		if (true)
+		{
+
+			// xy yz xz xw yw
+			yield return new WaitForSeconds(waitBetween);
+
+			for (float prog = 0; prog <= 1; prog += rate / 360f)
+			{
+				float rad = Mathf.Lerp(0, 2 * Mathf.PI, prog);
+				t4d.localRotation[(int)Rot4D.xy] = t4d.localRotation[(int)Rot4D.yw] = t4d.localRotation[(int)Rot4D.xw] = t4d.localRotation[(int)Rot4D.xz] = t4d.localRotation[(int)Rot4D.yz] = t4d.localRotation[(int)Rot4D.zw] = rad;
 				yield return new WaitForFixedUpdate();
 			}
 		}
